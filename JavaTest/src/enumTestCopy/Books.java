d52aab814635018ff256e78ee02db11fa20bd875
Ei Kay Khine <topnetspice@gmail.com>
2017-05-03 16:45:51 +0630

    enumTestCopy commit from test-branch

---
+++ JavaTest/src/enumTestCopy/Books.java
@@ -0,0 +1,7 @@
+package enumTestCopy;
+
+public enum Books {
+	HARRY_POTTER,
+    GAME_OF_THRONES,
+    DRAGONLANCE;
+}
---
+++ JavaTest/src/enumTestCopy/EnumTest.java
@@ -0,0 +1,32 @@
+package enumTestCopy;
+
+import java.awt.GraphicsEnvironment;
+
+public class EnumTest {
+
+	public static void main(String[] args) {
+		Books dragonlance = Books.DRAGONLANCE;
+
+		// toString() method could be used to get their string representation.
+		System.out.println(dragonlance.toString());
+
+		// they can use '==' operator to check their equality
+		if (dragonlance == Books.DRAGONLANCE) {
+			System.out.println(true);
+		}
+		
+	}
+
+	// Enums are compiled-time type safe
+	public String greet(Languages lang) {
+		if (lang == Languages.ENGLISH) {
+			return "English";
+		} else if (lang == Languages.GERMAN) {
+			return "Hallo";
+		} else if (lang == Languages.FRENCH) {
+			return "Salut";
+		} else {
+			return "Selam";
+		}
+	}
+}
